0b5b8bbc1ca2d88ca81f0af13d25f4fcfec40ac4
lichpeko <luke.ichpekov@gmail.com>
2025-03-06 21:50:39 -0500

    ok

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+        <div className='h-1/3 container' >
+        <h1 className='text-white'>
+        Luke Ichpekov
+        </h1>
+        </div>
+  )
+}
