e0643b1e17da744b987e715a3e8a8d7f4e0b36ff
lichpeko <luke.ichpekov@gmail.com>
2025-03-07 22:28:44 -0500

    ok closer

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import avatar from '../avatar.jpg';
 
 export default function Header() {
   return (
-        <div className='h-1/3 container' >
-        <h1 className='text-white'>
-        Luke Ichpekov
-        </h1>
+        <div className='h-1/3 container flex justify-center p-20' >
+        <img className="rounded-full w-24 h-24" src={avatar} alt="Avatar" />
+        <div className='m-3 mx-5'>
+          <h1 className='text-white text-2xl'>
+          Luke Ichpekov
+          </h1>
+          <h2 className='text-white text-base'>
+          Software Engineer
+          </h2>
+        </div>
         </div>
   )
 }
